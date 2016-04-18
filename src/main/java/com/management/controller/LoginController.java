715f5e499a869ce823eed29e8b45cbc536d664e8
liuxinyan <924548894@qq.com>
2016-04-18 14:20:37 +0800

    project structrue

---
+++ src/main/java/com/management/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by xinyan.liu on 2016/4/18.
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+}
